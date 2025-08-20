cc08ce0fd575eac1b29f5560be6b998b63b8999a
Vaibhav <vaibhavkumar20241@gmail.com>
2025-08-20 19:12:15 +0530

    b-up

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 function change(){
+
+    const hiding = document.getElementById("hiding")
     const slider = document.getElementById('slider')
     slider.classList.add("slider")
+    hiding.textContent="HBD!! Too fast? huh??"
+    setTimeout(() => {
+         hiding.textContent="No problem!!"
+        slider.style.animationDuration="20s"
+        slider.style.animationIterationCount= "infinite"
+        
+    }, 4000);
+
 }
