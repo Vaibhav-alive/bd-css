912e2f322f8ae4fdfb9797ed052e81ef3670be23
Vaibhav <vaibhavkumar20241@gmail.com>
2025-08-22 14:30:46 +0530

    final

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ function change(){
     const hiding = document.getElementById("hiding")
     const slider = document.getElementById('slider')
     slider.classList.add("slider")
-    hiding.textContent="HBD!! Too fast? huh??"
+    hiding.textContent="HBD!! Sorry bhai thoda late ho gaya.. Too fast? huh??"
     setTimeout(() => {
-         hiding.textContent="No problem!!"
+         hiding.textContent="No problem!!, keep hovering!!!"
         slider.style.animationDuration="20s"
         slider.style.animationIterationCount= "infinite"
         
